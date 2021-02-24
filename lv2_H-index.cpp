15909e2e33f4c016c7674d82580c4771464a56ae
durumi99 <kms990615@naver.com>
2021-02-24 20:58:52 +0900

    programmers

---
+++ lv2_H-index.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    sort(citations.begin(),citations.end(),greater <>());
+    for(int i = 0 ; i < citations.size() ; i++)
+        if(citations[i] <= i)
+            return i;          
+}
