1f75c060176efc55e47803101aeec2319fcaa622
durumi99 <kms990615@naver.com>
2022-01-16 18:42:01 +0900

    baekjoon

--- 17144.cpp
+++ 17144.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
 
 using namespace std;
 
--- 2096.cpp
+++ 2096.cpp
@@ -7,12 +7,13 @@ int num[100000][3];
 pair<int,int> dp[3];
 int main() {
     cin >> n;
-    for(int i = 0 ; i < n ; i++)
+    for(int i = 0 ; i < 3 ; i++){
+        cin >> num[0][i];
+        dp[i].first = dp[i].second = num[0][i];
+    }
+    for(int i = 1 ; i < n ; i++){
         for(int j = 0 ; j < 3 ; j++)
             cin >> num[i][j];
-    for(int i = 0 ; i < 3 ; i++)
-        dp[i].first = dp[i].second = num[0][i];
-    for(int i = 1 ; i < n ; i++){
         pair<int,int> tmp[3];
         // 0
         tmp[0].first = max(dp[0].first,dp[1].first) + num[i][0];
@@ -26,11 +27,11 @@ int main() {
         
         for(int j = 0 ; j < 3 ; j++)
         	dp[j] = tmp[j];
-        
     }  
     int maxScore = max( max(dp[0].first,dp[1].first),dp[2].first);
     int minScore = min( min(dp[0].second,dp[1].second),dp[2].second);
         
     cout << maxScore << ' ' << minScore << '\n';
+    
     return 0;
 }
