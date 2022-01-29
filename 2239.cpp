ada7dbc6ae1590556c44bc65a9bf64cade0898cb
durumi99 <kms990615@naver.com>
2022-01-29 18:30:13 +0900

    baekjoon

---
+++ 2239.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[9][9];
+bool row[9][9];
+bool col[9][9];
+bool square[9][9];
+void output(){
+    for(int i = 0 ; i < 9 ; i++){
+        for(int j = 0 ; j < 9 ; j++)
+            cout << arr[i][j];
+        cout << '\n';
+    }
+}
+void bt(int cnt){
+    int x = cnt / 9;
+    int y = cnt % 9;
+    
+    if(cnt == 81){ // 스도쿠가 완성되면 출력
+        output();
+        exit(0);
+    }
+    if(arr[x][y] == 0){
+        for(int i = 1 ; i <= 9 ; i++){
+            if(!row[x][i] && !col[y][i] && !square[ (x / 3) * 3 + (y / 3) ][i]){
+                row[x][i] = true;
+                col[y][i] = true;
+                square[ (x / 3) * 3 + (y / 3) ][i] = true;
+                arr[x][y] = i;
+                bt(cnt+1);
+                arr[x][y] = 0;
+                row[x][i] = false;
+                col[y][i] = false;
+                square[ (x / 3) * 3 + (y / 3) ][i] = false;
+                
+            }
+        }
+    }
+    else
+        bt(cnt+1);
+}
+void input(){
+    for(int i = 0 ; i < 9 ; i++)
+        for(int j = 0 ; j < 9 ; j++){
+            scanf("%1d",&arr[i][j]);
+            if(arr[i][j]){
+                row[i][arr[i][j]] = true;
+                col[j][arr[i][j]] = true;
+                square[( i / 3) * 3 + ( j / 3 ) ][arr[i][j]] = true;
+            }
+        }
+}
+int main() {
+    input();
+    bt(0);
+    return 0;
+}
---
+++ 2467.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX 101010
+#define INF 2e9
+using namespace std;
+int n;
+int arr[MAX];
+
+void f(){
+    int start = 0, end = n-1;
+    int answer1 = 0,answer2 = n-1;
+    int sum = INF;
+    while(start < end){
+        if(arr[start] + arr[end] == 0){ // 합이 0 이면 바로 답 출력
+            answer1 = start;
+            answer2 = end;
+            break;
+        }
+        if(abs(arr[start] + arr[end]) < sum){
+            sum = abs(arr[start] + arr[end]);
+            answer1 = start;
+            answer2 = end;
+        }
+        if(abs(arr[start+1] + arr[end]) < abs(arr[start] + arr[end-1])){
+            start++;
+        }
+        else{
+            end--;
+        }
+        if(start == end)
+            break;
+    }
+    cout << arr[answer1] << ' ' << arr[answer2];
+}
+int main() {
+    cin >> n;
+    for(int i = 0 ; i < n ; i++)
+        cin >>arr[i];
+    f();
+    return 0;
+}
