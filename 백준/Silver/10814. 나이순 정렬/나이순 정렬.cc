9e25981d589add8e643964e23343043346a98d6d
durumi99 <43162188+durumi99@users.noreply.github.com>
2023-01-17 14:57:07 +0900

    [Silver V] Title: 나이순 정렬, Time: 100 ms, Memory: 7904 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -1,28 +1,19 @@
-#include <vector>
-#include <algorithm>
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+
 using namespace std;
-
-bool comp(pair<int,string> a, pair<int, string> b) {
-    return a.first < b.first;
+bool cmp(pair <int,string> a, pair <int,string> b){
+  return a.first < b.first;
 }
-
 int main() {
-	int n ;
-	cin >> n;
-    pair< int, string> test = make_pair(1, "asb");
-    vector<pair<int, string>> vv;
-	int tmp1;
-    string tmp2;
-    for (int i = 0; i < n; i++) {
-    	cin >> tmp1 >> tmp2;
-        vv.push_back(make_pair(tmp1,tmp2));
-    }
-
-    stable_sort(vv.begin(), vv.end(),comp); 
-    for (int i = 0; i < vv.size(); i++) {
-        cout << vv[i].first << " " << vv[i].second << "\n";
-    }
-
-	return 0;
+  int n; cin >> n;
+  vector < pair<int, string> > v(n);
+  for(int i = 0 ; i < n ; i++)
+    cin >> v[i].first >> v[i].second ;
+  stable_sort(v.begin(), v.end(), cmp);
+  for(int i = 0 ; i < n ; i++)
+    cout << v[i].first << ' ' << v[i].second << '\n';
+  return 0;
 }
