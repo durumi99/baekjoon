b3ee5941b7452ddfd54e129089ab790973bea78b
durumi99 <43162188+durumi99@users.noreply.github.com>
2023-02-02 20:13:24 +0900

    [Platinum III] Title: 롤러코스터, Time: 288 ms, Memory: 8828 KB -BaekjoonHub

--- 백준/Platinum/2873. 롤러코스터/롤러코스터.cc
+++ 백준/Platinum/2873. 롤러코스터/롤러코스터.cc
@@ -1,113 +1,116 @@
 #include <iostream>
-#include <vector>
-#include <utility>
+#include <cmath>
+
+#define MAX 1001
 using namespace std;
+int R, C;
+int map[MAX][MAX];
 
+int minValue = MAX;
+int minY, minX;
+string result = "";
+
+void printR() {
+	for(int i = 0 ; i < R-1 ; i++) {
+		if(i % 2 == 0){
+			for(int j = 0 ; j < C-1 ; j++){
+				result += "R";
+			}
+			result += "D";
+		} else {
+			for(int j = 0 ; j < C-1 ; j++){
+				result += "L";
+			}
+			result += "D";
+		}
+	}
+	for(int j = 0 ; j < C-1 ; j++){
+		result += "R";
+	}
+}
+void printC() {
+	for(int i = 0 ; i < C-1 ; i++) {
+		if(i % 2 == 0){
+			for(int j = 0 ; j < R-1 ; j++){
+				result += "D";
+			}
+			result += "R";
+		} else {
+			for(int j = 0 ; j < R-1 ; j++){
+				result += "U";
+			}
+			result += "R";
+		}
+	}
+	for(int j = 0 ; j < R-1 ; j++){
+		result += "D";
+	}
+}
+
+void print() {
+	int newR = minY % 2 ? minY : minY-1; 
+
+	for(int i = 1 ; i < newR ; i++) {
+		for(int j = 0 ; j < C-1 ; j++) {
+			if(i % 2) {
+				result += "R";
+			} else {
+				result += "L";
+			}
+		}
+		result += "D";
+	}
+	int newC = minX;
+	
+	for(int i = 1 ; i < newC ; i++) {
+		if(i % 2) {
+			result += "DR";
+		} else {
+			result += "UR";
+		}
+	}
+	
+	for (int i = newC ; i < C ; i++) {
+			if (i % 2)
+				result += "RD";
+			else
+				result += "RU";
+		}
+		
+	
+	for (int i = newR + 2; i <= R; i++) {
+		result += 'D';
+		for (int j = 0; j < C-1 ; j++) {
+			if (i % 2)
+				result += 'L';
+			else
+				result += 'R';
+		}
+	}
+}
 int main() {
-	int R, C;
-
 	cin >> R >> C;
-
-	int **pleasure = new int*[R];
-	//2차원 배열 동적할당
-	for (int i = 0; i < R; i++) {
-		pleasure[i] = new int[C];
-	}
-
-	pair <int, int> blank;
-	int min = 1001; //1000최대
-	//배열 입력
-	for (int i = 0; i < R; i++) {
-		for (int j = 0; j < C; j++) {
-			cin >> pleasure[i][j];
-			if (min > pleasure[i][j] && (i + j) % 2 == 1) {
-				min = pleasure[i][j];
-				blank.first = i;
-				blank.second = j;
+	
+	for (int i = 1 ; i <= R ; i++) {
+		for(int j = 1 ; j <= C ; j++) {
+			cin >> map[i][j];
+			if((i+j) % 2 == 0)
+				continue;
+			if(minValue > map[i][j]) {
+				minValue = map[i][j];
+				minY = i;
+				minX = j;
 			}
 		}
 	}
-
-
-	string move;
-	int i, j;
-	if (R % 2 == 1) { //R이 홀수 일때
-		for (i = 0; i < R; i++) {
-			for (j = 0; j < C - 1; j++) {
-				if (i % 2 == 0)
-					move += 'R'; //오른쪽 방향
-				else
-					move += 'L'; //왼쪽 방향
-			}
-			if (i == R - 1)
-				break;
-			move += 'D'; //아래쪽 방향
-		}
+    
+	if (R % 2) {
+		printR();
+	} else if(C % 2) {
+		printC();
+	} else {
+		print();
 	}
-	else if (C % 2 == 1) { //C가 홀수 일때
-		for (i = 0; i < C; i++) {
-			for (j = 0; j < R - 1; j++) {
-				if (i % 2 == 0)
-					move += 'D'; //아래쪽 방향
-				else
-					move += 'U'; //위쪽 방향
-			}
-			if (i == C - 1)
-				break;
-			move += 'R'; //오른쪽 방향
-		}
-	}
-	else { //R, C 모두 짝수 일때
-		//모두 짝수일 때는 모든 칸을 지나갈수 없음. 무조건 한칸은 지나가지 않음
-		int r, c;
-		if (blank.first % 2 == 1)
-			r = blank.first - 1; //blank가 있는 윗줄까지
-		else
-			r = blank.first; //blank가 있는 줄까지 
-
-		//blank가 있는 줄 전까지  
-		for (int i = 0; i < r; i++) {
-			for (int j = 0; j < C - 1; j++) {
-				if (i % 2 == 0)
-					move += 'R';
-				else
-					move += 'L';
-			}
-			move += 'D';
-		}
-
-
-		//blank 전
-		c = blank.second;
-
-		for (int i = 0; i < c; i++) {
-			if (i % 2 == 0)
-				move += "DR";
-			else
-				move += "UR";
-		}
-
-		//blank 후
-		for (int i = c; i < C - 1; i++) {
-			if (i % 2 == 0)
-				move += "RD";
-			else
-				move += "RU";
-		}
-
-		for (int i = r + 2; i < R; i++) {
-			move += 'D';
-			for (int j = 0; j < C - 1; j++) {
-				if (i % 2 == 0)
-					move += 'L';
-				else
-					move += 'R';
-			}
-		}
-
-	}
-
-	cout << move << endl;
-
+	cout << result;
 	return 0;
 }
