b5f1989f5b1223a520d8b1a103986fbcfdb509bd
durumi99 <43162188+durumi99@users.noreply.github.com>
2023-02-12 15:25:29 +0900

    [Gold IV] Title: 세 수의 합, Time: 36 ms, Memory: 5232 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.cc
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -3,11 +3,8 @@
 #define MAX 1001
 using namespace std;
 
-long long arr[MAX];
-set <long long> s;
 int N; 
 vector <int> v, vv;
-long long ans = -1;
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -19,34 +16,26 @@ int main() {
         int tmp; cin >> tmp;
         v.push_back(tmp);
     }
-    
-    sort(v.begin(), v.end());
 
     for(int i = 0 ; i < N ; i++) {
         for(int j = i ; j < N ; j++) {
             vv.push_back(v[i]+v[j]);
         }
     }
+    
+    sort(v.begin(), v.end());
     sort(vv.begin(), vv.end());
     
-    // for(int i = N-1 ; i >= 3 ; i--) {
-    //     for(int j = i-1 ; j >= 2 ; j--) {
-    //         if(binary_search(vv.begin(), vv.end(), v[i]- v[j])) {
-    //             cout << v[i];
-    //             return 0;
-    //         }
-    //     }
-    // }
-    
-    int result = 0;
-    for(int i=0;i<N;i++){
-        for(int j=i+1;j<N;j++){
-            if(binary_search(vv.begin(), vv.end(), v[j]-v[i])){
-                result = max(result, v[j]);
+    for(int i = N-1 ; i >= 0 ; i--) {
+        for(int j = i ; j >= 0 ; j--) {
+            int target = v[i] - v[j];
+            if(binary_search(vv.begin(), vv.end(), target)) {
+                cout << v[i];
+                return 0;
             }
         }
     }
+    
 
-    cout<<result;
     return 0;
 }
