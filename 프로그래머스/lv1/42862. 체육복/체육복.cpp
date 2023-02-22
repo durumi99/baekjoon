55901a7c8de0eea0aa9d94747bbf753fc1e0dda5
durumi99 <43162188+durumi99@users.noreply.github.com>
2023-02-22 20:42:41 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = 0;
+    answer = n - lost.size();
+    sort(lost.begin(), lost.end());
+    sort(reserve.begin(),reserve.end());
+    
+    for(int i = 0 ; i < lost.size(); i++) {
+        auto found = find(reserve.begin(), reserve.end(), lost[i]);
+        
+        if(found != reserve.end()) {
+            reserve.erase(found);
+            lost.erase(lost.begin()+i);
+            i--;
+            answer++;
+        }
+    }
+    for(int i = 0 ; i < lost.size(); i++) {
+//         auto found = find(reserve.begin(), reserve.end(), lost[i]);
+        
+//         if(found != reserve.end()) {
+//             reserve.erase(found);
+//             answer++;
+//             continue;
+//         }
+        
+        auto found = find(reserve.begin(), reserve.end(), lost[i]-1);
+        
+        if(found != reserve.end()) {
+            reserve.erase(found);
+            answer++;
+            continue;
+        }
+        
+        found = find(reserve.begin(), reserve.end(), lost[i]+1);
+        
+        if(found != reserve.end()) {
+            reserve.erase(found);
+            answer++;
+            continue;
+        }
+        
+    }
+    
+    return answer;
+}
