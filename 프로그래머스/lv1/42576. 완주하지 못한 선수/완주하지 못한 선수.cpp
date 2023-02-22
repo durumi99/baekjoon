54691df50972e375abc4af1c5c9bf6df3b32b5a1
durumi99 <43162188+durumi99@users.noreply.github.com>
2023-02-22 19:35:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 120.46 ms, Memory: 32.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    map <string, int> m;
+    for(auto ele : participant) {
+        m[ele]++;
+    }
+    for(auto ele : completion) {
+        m[ele]--;
+    }
+    for(auto it = m.begin() ; it != m.end(); it++) {
+        if(it->second)
+            answer += it->first;
+    }
+    return answer;
+}
