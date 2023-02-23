88f6516abf7afbb9ad7398e7ec7df1b8a6ca218c
durumi99 <43162188+durumi99@users.noreply.github.com>
2023-02-23 17:22:04 +0900

    [level 3] Title: N으로 표현, Time: 6.18 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.cpp
@@ -1,42 +1,40 @@
-#include <bits/stdc++.h>
-
-// #define MAX 160000
-#define MAX 8
-#define INF 987654321
+#include <string>
+#include <unordered_set>
 
 using namespace std;
 
-set <int> dp[MAX];
-void makeDp(int N, int number) {
-    int num = 0;
-    for(int i = 0 ; i < MAX ; i++) {
-        num = num*10 + N;
-        dp[i].insert(num);
+int solution(int N, int number) {
+    int answer = -1; // 최솟값이 8보다 크면 -1을 return
+    unordered_set<int> s[8]; // 정렬할 필요 X, 중복제거는 필요 - unordered_set 사용
+    
+    int sum = 0;
+    for(int i = 0; i < 8; ++i){ // 각 항목에다가 N, NN, NNN... 넣어줌
+        sum = 10*sum + N;
+        s[i].insert(sum);
     }
     
-    for(int i = 1 ; i < MAX ; i++) {
-        for(int j = 0 ; j < i ; j++) {
-            for(auto a : dp[j]) {
-                for(auto b : dp[i - j - 1]) {
-                    dp[i].insert(a + b);
-                    dp[i].insert(a - b);
-                    dp[i].insert(a * b);
+    for (int i = 1; i < 8; ++i){
+        for(int j = 0; j < i; ++j){
+            for(int a : s[j]){
+                for(int b : s[i-j-1]){
+                    s[i].insert(a+b);
+                    s[i].insert(a-b);
+                    s[i].insert(a*b);
                     if(b != 0)
-                        dp[i].insert(a / b);
+                        s[i].insert(a/b);
                 }
             }
         }
     }
-}
-int solution(int N, int number) {
-    int answer = -1;
-    makeDp(N, number);
     
-    for(int i = 0 ; i < MAX ; i++) {
-        if(dp[i].count(number)) {
+    // set을 채웠으니 number가 set에 들어가 있는지 여부만 확인하면 된다!
+    for(int i = 0; i < 8; ++i){
+        if(s[i].find(number) != s[i].end()){
             answer = i + 1;
             break;
-        }
+        }  
     }
+        
+    
     return answer;
 }
