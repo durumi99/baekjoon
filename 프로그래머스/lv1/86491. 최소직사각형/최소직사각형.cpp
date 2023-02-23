77cf0a7a1c398cc84cf15b3ebd02285798afa34a
durumi99 <43162188+durumi99@users.noreply.github.com>
2023-02-23 18:08:55 +0900

    Update 최소직사각형.cpp

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.cpp
@@ -2,54 +2,15 @@
 
 using namespace std;
 
-int solution(vector<vector<int>> sizes) {
-    int answer = 987654321;
-    int w = -1;
-    pair <int, int> wIdx;
-    for(int i = 0 ; i < sizes.size() ; i++) {
-        // w = max(w, sizes[i][0]);
-        // w = max(w, sizes[i][1]);
-        if(sizes[i][0] > w) {
-            w = sizes[i][0];
-            wIdx = {i, 0};
-        }
-        if(sizes[i][1] > w) {
-            w = sizes[i][1];
-            wIdx = {i, 1};
-        }
+int solution(vector<vector<int>> sizes)
+{
+    int answer = 0;
+    int w = 0, h = 0;
+    for(auto size : sizes) {
+        w = max(w, min(size[0], size[1]));
+        h = max(h, max(size[0], size[1]));
     }
-    int h = -1;
-    for(int i = 0 ; i < sizes.size() ; i++) {
-        if(i != wIdx.first || 0 != wIdx.second) {
-            h = sizes[i][0];
-            bool check = true;
-            for(int j = 0 ; j < sizes.size(); j++) {
-                if(sizes[j][0] <= w && sizes[j][1] <= h)
-                    continue;
-                if(sizes[j][0] <= h && sizes[j][1] <= w)
-                    continue;
-                check = false;
-                break;
-            }
-            if(check)
-                answer = min(answer, w * h);
-        }
-        
-        if(i != wIdx.first || 1 != wIdx.second) {
-            h = sizes[i][1];
-            bool check = true;
-            for(int j = 0 ; j < sizes.size(); j++) {
-                if(sizes[j][0] <= w && sizes[j][1] <= h)
-                    continue;
-                if(sizes[j][0] <= h && sizes[j][1] <= w)
-                    continue;
-                check = false;
-                break;
-            }
-            if(check)
-                answer = min(answer, w * h);
-        }
-    }
-    
+    answer = w * h;
+
     return answer;
 }
