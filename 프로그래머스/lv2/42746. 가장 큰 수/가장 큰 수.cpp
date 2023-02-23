c01fe463bd90a08596591c0c7a3fe46728932531
durumi99 <43162188+durumi99@users.noreply.github.com>
2023-02-23 18:35:55 +0900

    Update 가장 큰 수.cpp

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -5,19 +5,18 @@ bool cmp(int a, int b) {
     string strA = to_string(a);
     string strB = to_string(b);
     
-    string tmp1 = strA + strB;
-    string tmp2 = strB + strA;
-    
-    return tmp1 > tmp2;
+    return strA + strB > strB + strA;
 }
 string solution(vector<int> numbers) {
     string answer = "";
+    
     sort(numbers.begin(), numbers.end(), cmp);
     for(auto number : numbers) {
         answer += to_string(number);
     }
-    if(answer[0] == '0' && answer[answer.length()-1] == '0')
-        return "0";
+    
+    if(answer[0] == '0')
+        answer = "0";
     
     return answer;
 }
