2d360d856c3026c571a36108ff6db9f32b21574c
durumi99 <43162188+durumi99@users.noreply.github.com>
2023-02-24 17:20:48 +0900

    [level 3] Title: 정수 삼각형, Time: 0.96 ms, Memory: 8.84 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define MAX 501
+using namespace std;
+int dp[MAX][MAX];
+int solution(vector<vector<int>> triangle) {
+    int answer = 0;
+    int height = triangle.size();
+    dp[0][0] = triangle[0][0];
+    for(int i = 1 ; i < triangle.size(); i++) {
+        for(int j = 0 ; j < triangle[i].size() ; j++) {
+            dp[i][j] = triangle[i][j];
+            if(j-1 < 0) {
+                dp[i][j] += dp[i-1][j];
+            } else if(j >= triangle[i-1].size()) {
+                dp[i][j] += dp[i-1][j-1];
+            } else {
+                dp[i][j] += max(dp[i-1][j-1], dp[i-1][j]);
+            }
+            
+        }
+    }
+    answer = *max_element(dp[height-1], dp[height-1]+height);
+    // for(int j = 0 ; j < 5 ; j++) {
+    //     for(int i = 0 ; i < 5 ; i++) {
+    //         cout << dp[j][i] << ' ';
+    //     }
+    //     cout << '\n';
+    // }
+    return answer;
+}
