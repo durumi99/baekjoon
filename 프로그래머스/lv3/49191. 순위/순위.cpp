b7c7221bbbf369a8cf8af8b3c1ed59f59f7e6ec3
durumi99 <43162188+durumi99@users.noreply.github.com>
2023-02-24 18:36:01 +0900

    [level 3] Title: 순위, Time: 1.64 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv3/49191. 순위/순위.cpp
+++ 프로그래머스/lv3/49191. 순위/순위.cpp
@@ -4,29 +4,45 @@
 using namespace std;
 
 int arr[MAX][MAX];
+int ranking[MAX];
 int solution(int n, vector<vector<int>> results) {
     int answer = 0;
+    for(int i = 1 ; i <= n ; i++)
+        ranking[i] = -1;
+    
     for(auto result : results) {
         arr[result[0]][result[1]] = 1;
+        arr[result[1]][result[0]] = -1;
     }
-            
     for(int i = 1 ; i <= n ; i++) {
+        int loseCnt = 0;
+        int winCnt = 0;
         for(int j = 1 ; j <= n ; j++) {
-            for(int k = 1 ; k <= n ; k++) {
-                if(arr[j][i] && arr[i][k])
-                    arr[j][k] = 1;
+            if(arr[i][j] == -1)
+                loseCnt++;
+            if(arr[i][j] == 1)
+                winCnt++;
+        }
+        if(loseCnt + winCnt == n-1) {
+            answer++;
+            ranking[i] = loseCnt+1;
+        }
+    }
+    for(int i = 1 ; i <= n ; i++) {
+        if(ranking[i] != -1) {
+            for(int j = 1 ; j <= n ; j++) {
+                if(arr[i][j] == 1) {
+                    ranking[j] = ranking[i] + 1;
+                }
+                if(arr[i][j] == -1) {
+                    ranking[j] = ranking[i] - 1;
+                }
+                
             }
         }
     }
-            
     for(int i = 1 ; i <= n ; i++) {
-        int cnt = 0;
-        for(int j = 1 ; j <= n ; j++) {
-            if(arr[i][j] || arr[j][i])
-                cnt++;
-        }
-        if(cnt == n-1)
-            answer++;
+        cout << ranking[i] << ' ';
     }
     return answer;
 }
