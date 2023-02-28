840b679ac93f0a08aaa6d26ec3e5d8c20850f6f7
durumi99 <43162188+durumi99@users.noreply.github.com>
2023-02-28 16:50:02 +0900

    [level 4] Title: 징검다리, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/43236. 징검다리/징검다리.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int distance, vector<int> rocks, int n) {
+    int answer = 0;
+    sort(rocks.begin(), rocks.end());
+    
+    int left = 1;
+    int right = distance; 
+    int mid;
+    
+    while(left <= right) {
+        mid = (left + right) / 2; // 거리의 최솟값 : mid
+        int cnt = 0, prev = 0;
+        
+        for(int i = 0 ; i < rocks.size() ; i++) {
+            if(rocks[i] - prev < mid) {
+                cnt++;
+            } else {
+                prev = rocks[i];
+            }
+        }
+        if(distance - prev < mid)   // 마지막 바위와 도착지점까지의 거리 체크
+            cnt++;
+        
+        if(cnt <= n) {
+            // answer = max(mid, answer);
+            answer = mid;
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    return answer;
+}
