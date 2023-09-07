5a587e2f483c74b94fdfad2af0541902bbb55059
뚜루미 <kms990615@naver.com>
2023-09-07 15:11:38 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.cc
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.cc
@@ -2,11 +2,6 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int visited[101];
-int x[101], y[101]; 
-int n; 
-int homeX, homeY;
-int pentaX, pentaY; 
 
 bool canMove(int startX, int startY, int destX, int destY) {
     int dist = abs(startX - destX) + abs(startY - destY);
@@ -17,46 +12,47 @@ bool canMove(int startX, int startY, int destX, int destY) {
     
     return false;
 }
-
-bool dfs(int curX, int curY) {
-    if(canMove(curX, curY, pentaX, pentaY)) {
-        return true;
-    }
-    vector <bool> res;    
-    for(int i = 0 ; i < n ; i++) {
-        if(!visited[i] && canMove(curX, curY, x[i], y[i])) {
-            visited[i] = 1;
-            res.push_back(dfs(x[i], y[i]));
-        }
-    }
-    for(int i = 0 ; i < res.size() ; i++) {
-        if(res[i])
-            return true;
-    }
-    
-    return false;
-}
 int main() {
+    ios_base::sync_with_stdio(0);
+    cout.tie(0); cin.tie(0);
+    
     int TC ; cin >> TC;
     while(TC--) {
-        cin >> n;
-        
+        int n; cin >> n;
+        int x[n], y[n]; 
+        int visited[n] = {0, };
         bool result = false;
-        cin >> homeX >> homeY;
+        int homeX, homeY; cin >> homeX >> homeY;
         for(int i = 0 ; i < n ; i++) {
             cin >> x[i] >> y[i];
         }
-        cin >> pentaX >> pentaY;
-        result = dfs(homeX, homeY);
+        int pentaX, pentaY; cin >> pentaX >> pentaY;
         
+        queue < pair<int, int> > q;
+        q.push({homeX, homeY});
+        
+        while(!q.empty()) {
+            int curX = q.front().first;
+            int curY = q.front().second;
+            q.pop();
+            
+            if(canMove(curX, curY, pentaX, pentaY)) {
+                result = true;
+                break;
+            }
+            for(int i = 0 ; i < n ; i++) {
+                if(!visited[i] && canMove(curX, curY, x[i], y[i])) {
+                    visited[i] = 1;
+                    q.push({x[i], y[i]});
+                }
+            }
+        }
         if(result) {
             cout << "happy\n";
         } else {
             cout << "sad\n";
         }
         memset(visited, 0, sizeof(visited));
-        memset(x, 0, sizeof(x));
-        memset(x, 0, sizeof(y));
     }
     
     return 0;
