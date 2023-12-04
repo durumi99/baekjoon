3f6be1ebbcb39da3607e39bedb4d36cf5cc3aac7
Minsu Kim <kms990615@naver.com>
2023-12-04 22:25:01 +0900

    [level 2] Title: 짝지어 제거하기, Time: 22.95 ms, Memory: 46.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,17 @@
+function solution(s)
+{
+    var answer = -1;
+    const stack = []
+    for(let i = 0 ; i < s.length ; i++){
+        if(stack[stack.length - 1] === s[i]){
+            stack.pop()
+        }
+        else{
+            stack.push(s[i])
+        }
+    }
+
+    return stack.length === 0 ? 1 : 0
+
+    return answer;
+}
