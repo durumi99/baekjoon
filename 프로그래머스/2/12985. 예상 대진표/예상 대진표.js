3065f21e0c516533962ace7c018d67c590b572c6
Minsu Kim <kms990615@naver.com>
2024-05-24 21:57:11 +0900

    Update 예상 대진표.js

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,12 +1,11 @@
 function solution(n,a,b)
 {
-    var answer = 1;
-    let minN = Math.min(a, b);
-    let maxN = Math.max(a, b);
+    var answer = 0;
     
-    while(!((maxN - minN === 1) && (minN % 2) === 1)) {
-        minN = (minN + 1) >> 1;
-        maxN = (maxN + 1) >> 1;
+    while(a !== b) {
+        a = (a + 1) >> 1;
+        b = (b + 1) >> 1;
+        
         answer++;
     }
     return answer;
