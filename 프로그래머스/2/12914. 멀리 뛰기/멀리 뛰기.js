6a49316db78220b14ebee56e92e5bd1b18cf69f1
Minsu Kim <kms990615@naver.com>
2024-05-25 11:03:02 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.37 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = 0;
+    let dp = Array.from({length: 2222}, () => 0);
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for(let i = 3 ; i <= n ; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    }
+    answer = dp[n];
+    return answer;
+}
