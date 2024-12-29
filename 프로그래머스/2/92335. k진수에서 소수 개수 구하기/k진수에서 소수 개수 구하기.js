2d6c8892b2d5d9a6fbc49bab52ab775065ad21f3
Minsu Kim <kms990615@naver.com>
2024-12-29 21:15:56 +0900

    Update k진수에서 소수 개수 구하기.js

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,18 +1,18 @@
-
 function solution(n, k) {
     const str = n.toString(k)
     const regex = /[1-9]*/g;
-const arr = str.match(regex).filter(e => e !== '' && e !== '1');
-const result = []
-arr.forEach(e => {
-let isPrime = true;
-for(let i=2 ; i<= Math.sqrt(e);i++) {
-if(e%i === 0) return isPrime = false;
-}
-    if(isPrime) {
-    result.push(e)
-    isPrime = true;
-}
-})
-    return result.length
+    const arr = str.match(regex).filter(e => e !== '' && e !== '1');
+        
+    let answer = 0;
+    
+    arr.forEach(e => {        
+        for(let i = 2 ; i<= Math.sqrt(e);i++) {
+            if(e % i === 0) 
+                return;
+        }
+        
+        answer++;
+    })
+    
+    return answer;
 }
