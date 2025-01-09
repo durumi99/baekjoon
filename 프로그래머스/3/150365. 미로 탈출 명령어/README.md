32fdd0be9a232df0c85c5406973c9a77318db101
Minsu Kim <kms990615@naver.com>
2025-01-09 20:35:43 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 3.27 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/README.md
+++ 프로그래머스/3/150365. 미로 탈출 명령어/README.md
@@ -1,10 +1,10 @@
 # [level 3] 미로 탈출 명령어 - 150365 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150365?language=javascript#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150365) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.15 ms
+메모리: 37 MB, 시간: 3.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 1일 23:5:12
+2025년 01월 09일 20:35:40
 
 ### 문제 설명
 
--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.js
@@ -1,75 +1,48 @@
+let N, M, R, C;
+let answer = 'impossible';
+
+const dx = [1, 0, 0, -1];
+const dy = [0, -1, 1, 0];
+const dc = ['d', 'l', 'r', 'u'];
 
 function solution(n, m, x, y, r, c, k) {
-    const MAX = 51;
-    const dx = [1, 0, 0, -1];
-    const dy = [0, -1, 1, 0];
-    const dc = ['d', 'l', 'r', 'u'];
-
-    let N, M, X, Y, R, C, K;
-    let answer = '';
-    const dp = new Array(MAX).fill(null).map(() => new Array(MAX).fill(''));
-
-    function f(n, m, x, y, r, c, k) {
-        N = n;
-        M = m;
-        X = x;
-        Y = y;
-        R = r;
-        C = c;
-        K = k;
+    N = n;
+    M = m;
+    R = r;
+    C = c;
+    
+    if(getLength(y, x) % 2 !== k % 2) {
+        return answer;
     }
-
-    function isImpossible(x, y, remainK) {
-        const pathLen = Math.abs(x - R) + Math.abs(y - C);
-        if (Math.abs(pathLen - remainK) % 2 !== 0)
-            return true;
-        if (pathLen > remainK)
-            return true;
-        return false;
-    }
-
-    function cmpString(A, B) {
-        const len = Math.min(A.length, B.length);
-
-        for (let i = 0; i < len; i++) {
-            if (A[i] < B[i])
-                return true;
-        }
-
-        return false;
-    }
-
-    function dfs(curX, curY) {
-        const remainK = K - dp[curX][curY].length;
-
-        if (isImpossible(curX, curY, remainK)) {
-            return;
-        }
-
-        for (let i = 0; i < 4; i++) {
-            const ny = curY + dy[i];
-            const nx = curX + dx[i];
-
-            if (ny <= 0 || nx <= 0 || ny > M || nx > N)
-                continue;
-
-            const tmp = dp[curX][curY] + dc[i];
-            if (tmp.length > K)
-                continue;
-
-            if (cmpString(dp[nx][ny], tmp))
-                continue;
-
-            dp[nx][ny] = tmp;
-            dfs(nx, ny);
-        }
-    }
-
-    f(n, m, x, y, r, c, k);
-    if (isImpossible(x, y, k))
-        return "impossible";
-
-    dfs(x, y);
-    answer = dp[r][c];
+    dfs(y, x, k, '');
     return answer;
+}
+
+function dfs(curY, curX, curK, curRoute) {
+    if(answer !== 'impossible')
+        return;
+    if(getLength(curY, curX) > curK) {
+        return;
+    }
+    if(curK === 0) {
+        if(curX === R && curY === C) {
+            answer = curRoute;
+        }
+        
+        return;
+    }
+    
+    for(let i = 0 ; i < 4 ; i++) {
+        const nextY = curY + dy[i];
+        const nextX = curX + dx[i];
+        if(nextY < 1 || nextY < 1 || nextX > N || nextY > M) {
+            continue;
+        }
+        
+        dfs(nextY, nextX, curK - 1 , curRoute + dc[i]);
+    }
+}
+
+function getLength(curY, curX) {
+    return Math.abs(curX - R) + Math.abs(curY - C);
 }
