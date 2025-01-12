0ab34b7a89861be81a7286350aff4fbe55701e11
Minsu Kim <kms990615@naver.com>
2025-01-12 20:08:59 +0900

    [level 2] Title: 양궁대회, Time: 0.83 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.js
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.js
@@ -1,40 +1,41 @@
-let answer = [-1];
-let maxScoreDiff = 1;
+
 
 function solution(n, info) {
+    let answer = [-1];
+    let maxScoreDiff = 1;
+    
+    const dfs = (n, info, idx, aScore, rScore, result) => {
+        if (idx === info.length){
+            if(n > 0) {
+                result[idx - 1] += n;
+            }
+
+            if(rScore - aScore > maxScoreDiff) {
+                maxScoreDiff = rScore - aScore;
+                answer = result;
+            }
+
+            if(rScore - aScore === maxScoreDiff) {
+                const reversedRes = [...result].reverse();
+                const reversedAns = [...answer].reverse();
+                if(reversedAns < reversedRes) {
+                    answer = result;
+                }
+            }
+            return;
+        }
+    
+        if (n > info[idx]) {
+            result.push(info[idx] + 1);
+            dfs(n - info[idx] - 1, info, idx + 1, aScore, rScore + 10 - idx, result);
+            result.pop();
+        }
+
+        result.push(0);
+        dfs(n, info, idx + 1, aScore + (info[idx] && 10 - idx), rScore, [...result]);
+        result.pop();
+    };
+    
     dfs(n, info, 0, 0, 0, []);
     return answer;
 }
-
-function dfs(n, info, idx, aScore, rScore, result) {
-    if (idx === info.length){
-        if(n > 0) {
-            result[idx - 1] += n;
-        }
-        
-        if(rScore - aScore > maxScoreDiff) {
-            maxScoreDiff = rScore - aScore;
-            answer = result;
-        }
-        
-        if(rScore - aScore === maxScoreDiff) {
-            const reversedRes = [...result].reverse();
-            const reversedAns = [...answer].reverse();
-            if(reversedAns < reversedRes) {
-                answer = result;
-            }
-        }
-        return;
-    }
-    
-    if (n > info[idx]) {
-        result.push(info[idx] + 1);
-        dfs(n - info[idx] - 1, info, idx + 1, aScore, rScore + 10 - idx, [...result]);
-        result.pop();
-    }
-    
-    result.push(0);
-    dfs(n, info, idx + 1, aScore + (info[idx] && 10 - idx), rScore, [...result]);
-    result.pop();
-    
-};
