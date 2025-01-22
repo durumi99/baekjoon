e485c753d0334bba521a5a79c9065c35e7804627
Minsu Kim <kms990615@naver.com>
2025-01-22 22:57:46 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 123.03 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.js
@@ -5,10 +5,9 @@ function solution(land) {
     const rows = land.length;
     const cols = land[0].length;
     const visited = Array.from({ length: rows }, () => Array(cols).fill(false));
-    const sectionMap = {};  // { sectionId: 해당 섹션의 총 기름양 }
+    const sectionMap = new Map();  // sectionId: 해당 섹션의 총 기름양
     let sectionId = 1;
 
-    // 🔹 BFS로 영역별 석유량 계산
     function bfs(y, x) {
         const queue = [[y, x]];
         visited[y][x] = sectionId;
@@ -31,8 +30,7 @@ function solution(land) {
                 positions.push([ny, nx]);
             }
         }
-
-        // 🔹 석유량을 저장하고, 해당 섹션에 속한 모든 좌표에 동일한 값 저장
+        
         sectionMap[sectionId] = count;
         positions.forEach(([y, x]) => {
             visited[y][x] = sectionId;  // 동일한 섹션 ID로 표시
@@ -41,7 +39,6 @@ function solution(land) {
         sectionId++;
     }
 
-    // 🔹 전체 지도 탐색하며 BFS 수행 (중복 탐색 방지)
     for (let y = 0; y < rows; y++) {
         for (let x = 0; x < cols; x++) {
             if (land[y][x] === 1 && !visited[y][x]) {
@@ -52,7 +49,6 @@ function solution(land) {
 
     let maxOil = 0;
 
-    // 🔹 각 열(column)에서 최대 석유량 계산
     for (let x = 0; x < cols; x++) {
         let columnOil = 0;
         const countedSections = new Set();
