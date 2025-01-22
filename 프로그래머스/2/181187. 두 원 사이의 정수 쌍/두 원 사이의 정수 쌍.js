51bc62e3c05ed035553646c5614c3372de9c694a
Minsu Kim <kms990615@naver.com>
2025-01-22 22:54:24 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 13.09 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.js
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.js
@@ -2,8 +2,8 @@ function solution(r1, r2) {
     let answer = 0;
 
     for (let x = 1; x <= r2; x++) {
-        let maxY = Math.floor(Math.sqrt(r2 * r2 - x * x)); // 큰 원 내부의 y 좌표 최대값
-        let minY = (r1 * r1 - x * x >= 0) ? Math.ceil(Math.sqrt(r1 * r1 - x * x)) : 0; // NaN 방지
+        const maxY = Math.floor(Math.sqrt(r2 * r2 - x * x)); // 큰 원 내부의 y 좌표 최대값
+        const minY = (r1 >= x) ? Math.ceil(Math.sqrt(r1 * r1 - x * x)) : 0; // NaN 방지
 
         answer += (maxY - minY + 1);
     }
