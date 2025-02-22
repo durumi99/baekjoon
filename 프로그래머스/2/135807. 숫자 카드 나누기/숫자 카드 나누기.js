8bb20a4407acc79ed7e9be45ae01b7927254faaf
Minsu Kim <kms990615@naver.com>
2025-02-22 13:47:48 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 1191.50 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -0,0 +1,22 @@
+function solution(arrayA, arrayB) {
+    var answer = 0;
+
+    const canDivideAll = (arr, N) => arr.every((el) => el % N === 0);
+    const canDivideNone = (arr, N) => arr.every((el) => el % N !== 0);
+
+    let minA = Math.min(arrayA.sort()[0]);
+    let minB = Math.min(arrayB.sort()[0]);
+    let maxN = Math.max(minA, minB);
+
+    while (maxN > 0) {        
+        if (canDivideAll(arrayA, maxN) && canDivideNone(arrayB, maxN)) {
+            return maxN;
+        }
+        if (canDivideAll(arrayB, maxN) && canDivideNone(arrayA, maxN)) {
+            return maxN;
+        }
+        maxN--;
+    }
+
+    return answer;
+}
