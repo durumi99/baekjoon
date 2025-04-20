e03924f71e323e62c142ae2cd94a377518652ae3
Minsu Kim <kms990615@naver.com>
2025-04-21 00:28:21 +0900

    [level 2] Title: 완전범죄, Time: 0.38 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.js
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.js
@@ -1,30 +1,4 @@
 function solution(info, n, m) {
-    var answer = -1;
-    const rate = info.map((el) => [...el, el[0] / el[1]]).sort((a, b) => b[2] - a[2] || b[1] - a[1]);
-    const visited = Array(rate.length).fill(false);
-    
-    let a = 0, b = 0;
-    
-    rate.forEach((el, idx) => {
-        if(b + el[1] < m) {
-            b += el[1];
-            visited[idx] = true;
-        }
-    });
-    
-    rate.forEach((el, idx) => {
-        if(visited[idx]) {
-            return;
-        }
-        
-        if(a + el[0] < n) {
-            a += el[0];
-            visited[idx] = true;
-        }
-    });
-    
-    if(visited.some((el) => !el)) {
-        return -1;
-    }
-    return a;
+    var answer = 0;
+    return answer;
 }
