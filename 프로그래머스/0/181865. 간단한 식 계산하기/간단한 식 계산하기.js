ddef89775b33cb686c8f0511db09951425f924a2
Minsu Kim <minsu.kim39@cj.net>
2025-10-09 18:59:38 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,11 @@
+function solution(binomial) {
+    const [a, op, b] = binomial.split(' ')
+    
+    if(op === '+') {
+        return ~~a + ~~b;
+    } else if(op === '-') {
+        return ~~a - ~~b;
+    } else {
+        return ~~a * ~~b;
+    }
+}
