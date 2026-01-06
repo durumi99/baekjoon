9411008b63abd784e34f29d7d8a76b74d4d2c1ef
Minsu Kim <minsu.kim39@cj.net>
2026-01-06 13:24:24 +0900

    Time: 3 ms (46.26%), Space: 65 MB (81.12%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -4,8 +4,8 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    k %= nums.length; // Handles both k < nums.length and k >= nums.length
-    if (k === 0) return; // Early exit if no rotation needed
+    k %= nums.length; 
+    if (k === 0) return; 
     const nums2 = nums.concat(nums);
     for (let i = 0; i < nums.length; i++) {
         nums[i] = nums2[(i + nums.length - k) % nums.length];
