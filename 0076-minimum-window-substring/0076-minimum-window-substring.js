f57294912446d4477d8e9104c01cd425daa269ae
Minsu Kim <minsu.kim39@cj.net>
2026-01-19 23:07:32 +0900

    Time: 12 ms (95.37%), Space: 56.7 MB (79.07%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.js
+++ 0076-minimum-window-substring/0076-minimum-window-substring.js
@@ -4,50 +4,47 @@
  * @return {string}
  */
 var minWindow = function(s, t) {
-    if (s.length < t.length) return "";
-
-    // 1. Map 대신 정수 배열 사용 (ASCII 128)
-    // 일반 Map보다 접근 속도가 압도적으로 빠름
-    const map = new Int32Array(128); 
-    let count = t.length; // 필요한 총 문자 개수
-    
-    // t의 문자 빈도수 기록
-    for (let i = 0; i < t.length; i++) {
-        map[t.charCodeAt(i)]++;
+    const bucket = new Int32Array(128);
+    let left=0,right=0;
+    let min = Infinity;
+    let haveCount=0;
+    let needTotal=t.length;
+    let startIndex = 0;
+    for(let char of t){
+        bucket[char.charCodeAt()]++;
     }
 
-    let minLen = Infinity;
-    let minStart = 0;
-    let left = 0;
-    let right = 0;
-    
-    // s.length 프로퍼티 접근 비용 아끼기 위해 지역 변수화
-    const sLen = s.length; 
-
-    while (right < sLen) {
-        // 2. 오른쪽 확장
-        // s[right] 문자의 카운트를 깎음. 
-        // 깎기 전 값이 0보다 컸다면, t에 포함된 '필요한 문자'를 찾았다는 뜻 -> count 감소
-        if (map[s.charCodeAt(right++)]-- > 0) {
-            count--;
+    while(right<s.length){
+        let rc = s.charCodeAt(right);
+        
+        if(bucket[rc]>0){
+            haveCount++;
         }
+        bucket[rc]--;
 
-        // 3. 유효 윈도우 축소 (count가 0이면 모든 조건 만족 상태)
-        while (count === 0) {
-            // 현재 윈도우 길이 갱신 (right는 이미 1 증가된 상태이므로 right - left가 길이)
-            if (right - left < minLen) {
-                minLen = right - left;
-                minStart = left;
+        while(haveCount==needTotal){
+            
+            if(min>(right-left+1)){
+                min=(right-left+1);
+                startIndex=left;
+            }
+            
+            let lc = s.charCodeAt(left);
+            bucket[lc]++;
+            if(bucket[lc]>0){
+                haveCount--;
             }
 
-            // 4. 왼쪽 축소
-            // s[left] 문자를 다시 뱉어냄 (카운트 증가)
-            // 증가시킨 값이 0보다 커진다면(즉 1이 된다면), 방금 뱉은 게 '필요했던 문자'라는 뜻 -> count 증가 (조건 깨짐)
-            if (map[s.charCodeAt(left++)]++ === 0) {
-                count++;
-            }
+            left++
         }
+        
+        right++;
+    }    
+
+    if(min == Infinity){
+        return "";
     }
 
-    return minLen === Infinity ? "" : s.substring(minStart, minStart + minLen);
+    return s.substring(startIndex,startIndex+min);
+
 };
