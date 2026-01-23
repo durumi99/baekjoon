d4c70497b698c09f6108f51088a2f583b463804b
Minsu Kim <minsu.kim39@cj.net>
2026-01-23 22:14:16 +0900

    Time: 0 ms (100%), Space: 53.6 MB (69.41%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.js
+++ 0290-word-pattern/0290-word-pattern.js
@@ -4,29 +4,27 @@
  * @return {boolean}
  */
 var wordPattern = function(pattern, s) {
-    const arr = [[...pattern], s.split(' ')];
-    const len = arr[0].length;
-    if(len !== arr[1].length) {
+    const arr = s.split(' ');
+    const len = pattern.length;
+    if(len !== arr.length) {
         return false;
     }
-    const checkPattern = (a, b) => {
-        const map = new Map();
-        for(let i = 0 ; i < len ; i++) {
-            const tmp1 = a[i];
-            const tmp2 = b[i];
-            const check = map.get(tmp1);
 
-            if(check) {
-                if(check !== tmp2) {
-                    return false;
-                }
-            } else {
-                map.set(tmp1, tmp2);
-            }
+   
+    const mapP = Object.create(null);
+    const mapS = Object.create(null);
+    
+    for(let i = 0 ; i < len ; i++) {
+        const charP = pattern[i];
+        const wordS = arr[i];
+        
+        if (mapP[charP] !== mapS[wordS]) {
+            return false;
         }
 
-        return true;    
+        mapP[charP] = i + 1;
+        mapS[wordS] = i + 1;
     }
 
-    return checkPattern(arr[0], arr[1]) && checkPattern(arr[1], arr[0]);
+    return true;    
 };
