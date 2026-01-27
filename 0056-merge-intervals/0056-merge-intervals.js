2a87806b819408627e5a99204690a5de89f691b5
Minsu Kim <minsu.kim39@cj.net>
2026-01-27 22:01:02 +0900

    Time: 7 ms (84.29%), Space: 63.9 MB (86.86%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -1,25 +1,51 @@
+// /**
+//  * @param {number[][]} intervals
+//  * @return {number[][]}
+//  */
+// var merge = function(intervals) {
+//     intervals.sort((a, b) => {
+//         // if(a[0] === b[0]) {
+//         //     return a[1] - b[1];
+//         // }
+//         return a[0] - b[0];
+//     });
+
+//     const answer = [];
+//     let [left, right] = intervals[0];
+//     for(let i = 1 ; i < intervals.length ; i++) {
+//         if(intervals[i][0] <= right) {
+//             right = Math.max(right, intervals[i][1]);
+//         } else {
+//             answer.push([left, right]);
+//             [left, right] = intervals[i];
+//         }
+//     }
+//     answer.push([left, right]); 
+//     return answer;
+// };
+
 /**
  * @param {number[][]} intervals
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    intervals.sort((a, b) => {
-        // if(a[0] === b[0]) {
-        //     return a[1] - b[1];
-        // }
-        return a[0] - b[0];
-    });
+    // sort incremental order of start_i
 
-    const answer = [];
-    let [left, right] = intervals[0];
-    for(let i = 1 ; i < intervals.length ; i++) {
-        if(intervals[i][0] <= right) {
-            right = Math.max(right, intervals[i][1]);
+    intervals.sort((a,b) => (a[0] - b[0]))
+
+    const result = []
+
+    let prev = intervals[0]
+
+    for (let i = 1; i < intervals.length; i++) {
+        const curr = intervals[i]
+        if (prev[1] >= curr[0]) {
+            prev[1] = Math.max(curr[1], prev[1])
         } else {
-            answer.push([left, right]);
-            [left, right] = intervals[i];
+            result.push(prev)
+            prev = curr
         }
     }
-    answer.push([left, right]); 
-    return answer;
+    result.push(prev)
+    return result
 };
