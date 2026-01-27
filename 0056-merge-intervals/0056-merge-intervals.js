56f908148ea1700b9d80a73e6d2ae8709a60be0e
Minsu Kim <minsu.kim39@cj.net>
2026-01-27 21:58:25 +0900

    Time: 11 ms (35.54%), Space: 67.4 MB (9.95%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -4,9 +4,9 @@
  */
 var merge = function(intervals) {
     intervals.sort((a, b) => {
-        if(a[0] === b[0]) {
-            return a[1] - b[1];
-        }
+        // if(a[0] === b[0]) {
+        //     return a[1] - b[1];
+        // }
         return a[0] - b[0];
     });
 
