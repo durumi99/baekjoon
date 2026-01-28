2a639029b9210b3cbec1b2ac3829078e87e9d64f
Minsu Kim <minsu.kim39@cj.net>
2026-01-28 22:42:55 +0900

    Time: 33 ms (31.35%), Space: 65.5 MB (22.7%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(s) {
+    let res = 0;
+	let sign = 1;
+	let num = 0;
+	const stack = [];
+
+    for (const c of s) {
+        if(c === ' ') {
+            continue;
+        }
+
+        if(!isNaN(c)){
+            num = num * 10 + Number(c);
+        } else if(c === '+') {
+            res += sign * num;
+            sign = 1;
+            num = 0;
+        } else if(c === '-') {
+            res += sign * num;
+            sign = -1;
+            num = 0;
+        } else if(c === '(') {
+            stack.push(res);
+            stack.push(sign);
+            res = 0;
+            sign = 1;
+        } else {
+            res += sign * num;
+            num = 0;
+            res *= stack.pop();
+            res += stack.pop();
+        }
+    }
+
+    res += sign * num;
+    return res;
+};
