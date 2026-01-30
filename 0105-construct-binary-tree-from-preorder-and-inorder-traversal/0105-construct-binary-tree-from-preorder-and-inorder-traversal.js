f883d539769a1964b5c57cb8eada47434051b72d
Minsu Kim <minsu.kim39@cj.net>
2026-01-31 00:56:48 +0900

    Time: 8 ms (44.73%), Space: 60.2 MB (47.74%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -12,27 +12,21 @@
  * @return {TreeNode}
  */
 var buildTree = function(preorder, inorder) {
-    const tree = new TreeNode(preorder[0]);
-    
-    const dfs = (cur, curPreorder, curInorder) => {
-        const rootIndex = curInorder.indexOf(curPreorder[0]);
-        cur.val = curPreorder[0];
-        if(curPreorder.length === 1 || rootIndex === -1) {
-            return;
+    let index = 0;
+    const dfs = (left, right) => {
+        if(left > right) {
+            return null;
         }
         
-        const left = curInorder.slice(0, rootIndex);
-        const right = curInorder.slice(rootIndex + 1);
-        if(left.length) {
-            cur.left = new TreeNode();
-            dfs(cur.left, curPreorder.slice(1, left.length + 1), left);
-        }
-        if(right.length) {
-            cur.right = new TreeNode();
-            dfs(cur.right, curPreorder.slice(1 + left.length), right);
-        }
+        const rootVal = preorder[index++];
+        const root = new TreeNode(rootVal);
+        const mid = inorder.indexOf(rootVal);
+
+        root.left = dfs(left, mid - 1);
+        root.right = dfs(mid + 1, right);
+
+        return root;
     }
-    dfs(tree, preorder, inorder);
-    
-    return tree;
+
+    return dfs(0, preorder.length - 1);
 };
