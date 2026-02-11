30b6a2bd458e510dd247913b12ff2a9fc5d98ddb
Minsu Kim <minsu.kim39@cj.net>
2026-02-11 22:20:34 +0900

    Time: 76 ms (5.53%), Space: 65.3 MB (5.05%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    let answer = true;
+    const arr = [];
+
+    const dfs = (cur) => {
+        if(!cur) return;
+        if(!answer) return;
+        if(cur.left) dfs(cur.left);
+        console.log(cur.val)
+        if(arr.length === 0 || cur.val > arr.at(-1)) arr.push(cur.val);
+        else answer = false;
+        if(cur.right) dfs(cur.right);
+    }
+    
+    dfs(root); 
+    console.log(arr)
+    return answer;
+};
